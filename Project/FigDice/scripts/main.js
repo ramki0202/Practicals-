96cf29347962d1b49611d329b7e821515b0ba61e
Ramki <ram.ramki0202@gmail.com>
2021-04-10 17:19:58 +0530

    TASK 2)to assign the value of random dice in the current score

--- Project/FigDice/scripts/main.js
+++ Project/FigDice/scripts/main.js
@@ -57,17 +57,6 @@ player2HighScore.textContent = 0;
 player1Score.textContent = 0;
 player2Score.textContent = 0;
 
-// var images = [],
-//   index = 0;
-// images[0] =
-//   "<a href = 'https://www.computerhope.com/'><img src='https://www.computerhope.com/banners/banner.gif' alt='Visit Computer Hope'></a>";
-// images[1] =
-//   "<a href = 'https://www.computerhope.com/history'><img src='https://www.computerhope.com/banners/banner2.gif' alt='Computer History'></a>";
-// images[2] =
-//   "<a href = 'https://www.computerhope.com/'><img src='https://www.computerhope.com/banners/banner3.gif' alt='Visit Computer Hope'></a>";
-// index = Math.floor(Math.random() * images.length);
-// document.write(images[index]);
-
 //Problem Solving
 /*
 
