d5c0e2c9b3ff34c29d4f15213ddb27921242c797
Ramki <ram.ramki0202@gmail.com>
2021-04-10 17:19:04 +0530

    TASK1): When user click RollDice button he will get random dice image

--- Project/FigDice/scripts/main.js
+++ Project/FigDice/scripts/main.js
@@ -24,13 +24,32 @@ overlay.addEventListener("click", function () {
 //variables diclaration
 const player1Score = document.querySelector(".player1-CurrentScore");
 const player2Score = document.querySelector(".player2-CurrentScore");
+let scoreAddition = 0;
 const player1HighScore = document.querySelector(".player1-highscore");
 const player2HighScore = document.querySelector(".player2-highscore");
-const dice = document.querySelector(".dice");
+let dices = document.getElementById("dice");
 const newGame = document.querySelector(".new-game");
 const rollDice = document.querySelector(".roll-dice");
 const hold = document.querySelector(".hold");
-let diceNumber = Math.trunc(Math.random() * 6) + 1;
+// let images = Math.trunc(Math.random() * 6) + 1;
+let images = [],
+  index = 0;
+images[0] = "/images/dice-1.png";
+images[1] = "/images/dice-2.png";
+images[2] = "/images/dice-3.png";
+images[3] = "/images/dice-4.png";
+images[4] = "/images/dice-5.png";
+images[5] = "/images/dice-6.png";
+
+rollDice.addEventListener("click", function () {
+  for (var i = 0; i <= 100; i++) {
+    index = Math.floor(Math.random() * images.length);
+    dice.src = images[index];
+    index++;
+  }
+  scoreAddition = Number(scoreAddition) + index;
+  player1Score.textContent = scoreAddition;
+});
 
 // logics1(if the starting point is score will be 0 and dice is hidden)
 player1HighScore.textContent = 0;
@@ -38,4 +57,31 @@ player2HighScore.textContent = 0;
 player1Score.textContent = 0;
 player2Score.textContent = 0;
 
+// var images = [],
+//   index = 0;
+// images[0] =
+//   "<a href = 'https://www.computerhope.com/'><img src='https://www.computerhope.com/banners/banner.gif' alt='Visit Computer Hope'></a>";
+// images[1] =
+//   "<a href = 'https://www.computerhope.com/history'><img src='https://www.computerhope.com/banners/banner2.gif' alt='Computer History'></a>";
+// images[2] =
+//   "<a href = 'https://www.computerhope.com/'><img src='https://www.computerhope.com/banners/banner3.gif' alt='Visit Computer Hope'></a>";
+// index = Math.floor(Math.random() * images.length);
+// document.write(images[index]);
+
 //Problem Solving
+/*
+
+Task: 
+    1). When user click RollDice button he will get random dice image
+    2). to assign the value of random dice in the current score
+    3). to add the current score in the same window 
+    4). if user get 1 value he will lose all his/her scores
+    5). if user click the hold button, his current score will be store in highscore board
+    6). so the trun will be change to another user same thing will happend for current user.
+    7). when any user get greater than or equal to 100 he/she will be Winner
+    8). when user click newgame all score will be zero.
+
+
+
+
+*/
