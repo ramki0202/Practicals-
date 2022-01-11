0ee63a92ae7fb2afa524ea95c88825faca674178
Ramki <ram.ramki0202@gmail.com>
2022-01-11 21:34:30 +0530

    FIrst React Project

---
+++ React/11.1.22/script.js
@@ -0,0 +1,8 @@
+const para = document.querySelector(".para");
+const btn = document.querySelector(".btn");
+btn.onclick = function btn() {
+  para.innerHTML = "Hey Ramki you have 60 more days to be a bachelor";
+};
+
+console.log(para);
+// alert("this is test");
